dcfd52490aaa82e6d2447041dea7df2847165f61
Gavin <pydev2007@gmail.com>
2026-07-27 08:59:02 -0600

    Added return and changed to httpRequest.

--- vars/github.groovy
+++ vars/github.groovy
@@ -4,21 +4,22 @@ def gitRepo(Map config) {
                     usernameVariable: 'USER',
                     passwordVariable: 'TOKEN'
                 )])  {
-        // Kept just in case there's no httpRequest for some reason.
+
+        sh """curl -L \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer ${TOKEN}" \
+        -H "X-GitHub-Api-Version: 2026-03-10" \
+        https://api.github.com/repos/${config.owner}/${config.repo}/${config.path}"""
+
+
+        // Kept in case httpRequest is prefered
         //
-        // sh """curl -L \
-        // -H "Accept: application/vnd.github+json" \
-        // -H "Authorization: Bearer ${TOKEN}" \
-        // -H "X-GitHub-Api-Version: 2026-03-10" \
-        // https://api.github.com/repos/${config.owner}/${config.repo}/${config.path}"""
-
-
-        def response = httpRequest(
-            url: "https://api.github.com/repos/${config.owner}/${config.repo}/${config.path}",
-            httpMode: 'GET',
-            customHeaders: ["Accept: application/vnd.github+json", "Authorization: Bearer ${TOKEN}", "X-GitHub-Api-Version: 2026-03-10"],
-            validResponseCodes: '200'
-        )
+        // def response = httpRequest(
+        //     url: "https://api.github.com/repos/${config.owner}/${config.repo}/${config.path}",
+        //     httpMode: 'GET',
+        //     customHeaders: ["Accept: application/vnd.github+json", "Authorization: Bearer ${TOKEN}", "X-GitHub-Api-Version: 2026-03-10"],
+        //     validResponseCodes: '200'
+        // )
 
         return response.content
     }
