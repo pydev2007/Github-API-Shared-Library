4927e394cb18f767ec082800dab1f568d9857534
Gavin <pydev2007@gmail.com>
2026-07-27 09:06:25 -0600

    sh does not return as standard out for some reason.

--- vars/github.groovy
+++ vars/github.groovy
@@ -5,12 +5,16 @@ def gitRepo(Map config) {
                     passwordVariable: 'TOKEN'
                 )])  {
 
-        def response = sh """curl -L \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer ${TOKEN}" \
-        -H "X-GitHub-Api-Version: 2026-03-10" \
-        https://api.github.com/repos/${config.owner}/${config.repo}/${config.path}"""
-
+        def response = sh(
+            script: """
+                        curl -sL \
+                        -H "Accept: application/vnd.github+json" \
+                        -H "Authorization: Bearer ${TOKEN}" \
+                        -H "X-GitHub-Api-Version: 2022-11-28" \
+                        "https://api.github.com/repos/${config.owner}/${config.repo}/${config.path}"
+                    """,
+                    returnStdout: true
+                ).trim()
 
         // Kept in case httpRequest is prefered
         //
