ee313a29a8e13e308240d41d2c39cf0aef4f4237
Gavin <pydev2007@gmail.com>
2026-07-27 13:41:25 -0600

    wrong var

--- vars/github.groovy
+++ vars/github.groovy
@@ -27,5 +27,5 @@ def gitRepo(Map config) {
 def repoDiscovery() {
     def defaultYamlText = libraryResource('repos.yaml')
     def config = readYaml text: defaultYamlText
-    echo defaultYamlText.discovery_config.repos
+    echo config.discovery_config.repos
 }
