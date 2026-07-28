cb1fdb93c8fd2e7dce4f45118ce0fee2f7b4b738
Gavin <pydev2007@gmail.com>
2026-07-28 10:22:29 -0600

    Initial commit..

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# jenkins-shared-library
-A collection of Groovy scripts that can be shared between different Jenkins jobs
+# Discovery Pipeline
+A simple shared library with a pipeline to check any commits are made after a release.
+
+## Additional Notes
+
+Repos may need to be specified inside the the Github App creds in Jenkins. Change the repo access strategy to "Specify accessible repositories" in Jenkins if it throws an access error in: your credential > advanced > Repository access strategy. Then, list your repositories in the repo section below.
--- vars/git.groovy
+++ vars/git.groovy
@@ -1,10 +1,17 @@
+/**
+ * Calls the github API and returns a JSON response.
+ *
+ * @param config A map that contains the config for the Github API:
+ *   - `owner` (String): Owner of the repo, such as an org or user. *Required*.
+ *   - `repo` (String): The repo to call in the API. *Required*.
+ *   - `path` (String): End of the url to call. *Required*.
+ */
 def gitRepo(Map config) {
     withCredentials([usernamePassword(
-                    credentialsId: 'JenkinsGTTest',
+                    credentialsId: 'GITHUB APP ID',
                     usernameVariable: 'USER',
                     passwordVariable: 'TOKEN'
                 )])  {
-
 
                 def response = sh(
                     script: """
@@ -22,11 +29,3 @@ def gitRepo(Map config) {
                 return json
     }
 }
-
-// def repoDiscovery() {
-//     def defaultYamlText = libraryResource('repos.yaml')
-//     def config = readYaml text: defaultYamlText
-//     for (i in config.discovery_config.repos) {
-//         def release = gitRepo()
-//     }
-// }
